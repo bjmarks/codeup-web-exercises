6f04d9c6a915967b937c335969d89d7e5ac6da00
Benjamin <bjmarks13@yahoo.com>
2022-06-01 14:58:12 -0500

    added Javascript folder

---
+++ JAVASCRIPT/js/External.js
@@ -0,0 +1,10 @@
+"use strict";
+console.log("Hello from external JavaScript");
+alert('Welcome to my Website!');
+let ask = prompt("what's your fouvrite color? " , "green");
+alert("Great, " + ask + " that's my favorite color too");
+
+
+// When the exercise asks you to use a number, instead use a prompt to ask the user for that number.
+// Use an alert to show the results of each problem.
+// Finally, commit the changes to your git repository, and push to GitHub.
