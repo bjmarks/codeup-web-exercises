d5f7f35249e28f9d3a89cf89c4a988579e90367c
Benjamin <bjmarks13@yahoo.com>
2022-06-08 15:44:22 -0500

    Fix linking src issues

--- JAVASCRIPT/js/planets-split.js
+++ JAVASCRIPT/js/planets-split.js
@@ -3,13 +3,6 @@
 
     var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
     var planetsArray;
-
-    /**
-     * TODO:
-     * Convert planetsString to an array, and save it in a variable named
-     * planetsArray.
-     * console.log planetsArray to check your work
-     */
     planetsArray = planetsString.split("|");
     console.log(planetsArray);
     /**
