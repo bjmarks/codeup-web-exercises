8e86ff1366224055f2c57f22d1d6c354671445f0
Benjamin Marks <bjmarks13@yahoo.com>
2022-07-08 10:02:50 -0500

    added konami code

---
+++ JQuery/Konami/test.js
@@ -0,0 +1,47 @@
+// body.addEventListener("keydown", moveBox);
+// function moveBox(e){
+// if (e.key === "w") {
+//             console.log(`${e.key} keypressed`);
+//             if(e.key == "W"){
+//                 up -= 5;
+//             }
+//             else{
+//                 up--;
+//            }
+//            console.log(box.style.top = `${up}px`);
+//            box.style.top = `${up}px`;
+//         }
+//         if (e.key === "s") {    
+//             console.log(`${e.key} keypressed`);
+//             if(e.shiftKey){
+//                 down -= 5
+//             }
+//             else{
+//                 down--
+//             }
+//             console.log(box.style.bottom = `${down}px`);
+//             box.style.bottom = `${down}px`;
+//         }
+//         if (e.key === "a") {
+//             console.log(`${e.key} keypressed`);
+//             if (e.shiftKey){
+//                 left -= 5;
+//             }
+//             else{
+//                 left--
+//             }
+//             console.log(box.style.left = `${left}px`);
+//             box.style.left = `${left}px`;
+//         }
+//         if (e.key === "d") {
+//             console.log(`${e.key} keypressed`);
+//             if(e.shiftKey){
+//                 right -= 5
+//             }
+//             else{
+//                 right--
+//             }
+//             console.log(box.style.right = `${right}px`);
+//             box.style.right = `${right}px`;
+//         }
+//     }
