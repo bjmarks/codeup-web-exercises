8fee177145a0e4db9f2796a8736f5b5ff681245c
Benjamin Marks <bjmarks13@yahoo.com>
2022-07-12 15:12:03 -0500

    cleaned up

--- JQuery/js/fetch.js
+++ JQuery/js/fetch.js
@@ -1,18 +1,10 @@
 "use strict";
-let tools = {};
 async function fetchData() {
-// TODO: Create an AJAX GET request for the file under data/inventory.json
-    const tableBody = document.querySelector("#insertProducts");
-    fetch("data/inventory.json").then(response => {
-        if (response.status !== 200 || !response.ok){
-            throw Error("erorr")
-        }
-        return response.json();
-            }).then(data => {
-                console.log(data);
-                const tools = data;
-                for(let i = 0; i < tools.length; i++) {
-                tableBody.innerHTML +=  `<tr><td>${tools[i].title}</td><td>${tools[i].quantity}</td><td>${tools[i].price}</td><td>${tools[i].categories}</td></tr>`;
-                }});    
-}
+ const tableBody = document.querySelector("#insertProducts");
+ fetch("data/inventory.json").then(response => {
+    if (response.status !== 200 || !response.ok){throw Error("erorr")}
+     return response.json();
+ }).then(data => { const tools = data;
+        for(let i = 0; i < tools.length; i++) {
+        tableBody.innerHTML +=  `<tr><td>${tools[i].title}</td><td>${tools[i].quantity}</td><td>${tools[i].price}</td><td>${tools[i].categories}</td></tr>`;}});}
 fetchData();
