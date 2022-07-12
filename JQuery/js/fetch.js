17aa8421aeec1272fbda7509d6a5a30229e7831c
Benjamin Marks <bjmarks13@yahoo.com>
2022-07-12 15:03:08 -0500

    done

--- JQuery/js/fetch.js
+++ JQuery/js/fetch.js
@@ -1,28 +1,18 @@
-(function() {
-    "use strict";
-    // TODO: Create an AJAX GET request for the file under data/inventory.json
-    const tools = await fetch("data/invetory.json")
-        .then(async function(response) {
-        if(response.status !== 200){
-            console.log("cannot read tools file")
-            return ""
+"use strict";
+let tools = {};
+async function fetchData() {
+// TODO: Create an AJAX GET request for the file under data/inventory.json
+    const tableBody = document.querySelector("#insertProducts");
+    fetch("data/inventory.json").then(response => {
+        if (response.status !== 200 || !response.ok){
+            throw Error("erorr")
         }
-        else {
-            return await response.json()
-        }
-        });
-        tools.forEach(function (tool, i) {
-            let tableBody = document.querySelector("#insertProducts");
-            tableBody.innerHTML =  `
-                <tr>
-                    <td>${tools[i].title}</td>
-                    <td>${tools[i].quantity}</td>
-                    <td>${tools[i].price}</td>
-                    <td>${tools[i].categories}</td>
-                </tr>`;
-    })
-    // TODO: Take the data from inventory.json and append it to the products table
-    //       HINT: Your data should come back as a JSON object; use console.log() to inspect
-    //             its contents and fields
-    //       HINT: You will want to target #insertProducts for your new HTML elements
-})();
+        return response.json();
+            }).then(data => {
+                console.log(data);
+                const tools = data;
+                for(let i = 0; i < tools.length; i++) {
+                tableBody.innerHTML +=  `<tr><td>${tools[i].title}</td><td>${tools[i].quantity}</td><td>${tools[i].price}</td><td>${tools[i].categories}</td></tr>`;
+                }});    
+}
+fetchData();
