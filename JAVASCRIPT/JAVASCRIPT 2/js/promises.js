3150fd68ca8821d48212d8851668ebd82dc1ff5d
Benjamin Marks <bjmarks13@yahoo.com>
2022-07-20 09:54:26 -0500

    js2 updated

---
+++ JAVASCRIPT/JAVASCRIPT 2/js/promises.js
@@ -0,0 +1,15 @@
+// let token = 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;
+fetch(url, {headers: {'Authorization': token}})
+
+
+
+// Create a function that accepts a GitHub username, and returns a promise that resolves returning just the date of the last commit that user made. Reference the github api documentation to achieve this.
+
+// Create Your Own Promise
+
+// Write a function named wait that accepts a number as a parameter, and returns a promise that resolves after the passed number of milliseconds.
+
+
+// wait(1000).then(() => console.log('You\'ll see this after 1 second'));
+// wait(3000).then(() => console.log('You\'ll see this after 3 seconds'));
+// 1. As a bonus make sure the promise resolves with the milliseconds in return, so you can make the console log message more dynamic.
